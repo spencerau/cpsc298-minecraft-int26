ba0baf8f292904f8eb233f0f0c342327f59d88c4
Spencer Au <spencerau96@gmail.com>
2026-01-13 00:21:43 -0800

    corgi block working

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -69,6 +69,17 @@ public class ExampleMod {
     public static final DeferredItem<BlockItem> RUBIKS_BLOCK_ITEM =
             ITEMS.registerSimpleBlockItem("rubiks_block", RUBIKS_BLOCK);
 
+    public static final DeferredBlock<Block> CORGI_DISPENSER_BLOCK = BLOCKS.registerSimpleBlock("corgi_dispenser_block",
+        BlockBehaviour.Properties.of()
+            .mapColor(MapColor.COLOR_YELLOW)
+            .strength(0.5f, 6.0f) // Hardness, Resistance
+            .lightLevel(state -> 4) // Emits light level 10
+            .sound(net.minecraft.world.level.block.SoundType.STONE) // Stone sound when stepped on or broken
+    );
+
+    public static final DeferredItem<BlockItem> CORGI_DISPENSER_BLOCK_ITEM =
+        ITEMS.registerSimpleBlockItem("corgi_dispenser_block", CORGI_DISPENSER_BLOCK);
+
     // CREATIVE TABS
 
     // NOTE: Add your block item below like "output.accept(CHEESE_BLOCK_ITEM.get())"
@@ -77,10 +88,11 @@ public class ExampleMod {
             CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup.examplemod")) // The translation key for the tab title
                     .withTabsBefore(CreativeModeTabs.COMBAT)
-                    .icon(() -> CHEESE_BLOCK_ITEM.get().getDefaultInstance())
+                    .icon(() -> CORGI_DISPENSER_BLOCK_ITEM.get().getDefaultInstance())
                     .displayItems((parameters, output) -> {
                         output.accept(CHEESE_BLOCK_ITEM.get()); // Your custom block item
                         output.accept(RUBIKS_BLOCK_ITEM.get());
+                        output.accept(CORGI_DISPENSER_BLOCK_ITEM.get()); // Your custom block item
                     }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
