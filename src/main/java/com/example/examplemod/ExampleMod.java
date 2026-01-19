68c5dbe21a6e8958639cea88d8f1fe0154cc308a
Spencer Au <spencerau96@gmail.com>
2026-01-18 20:35:00 -0800

    deleted assets/java code

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -4,24 +4,14 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -41,9 +31,6 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import net.minecraft.sounds.SoundEvent;
-
-
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ExampleMod.MODID)
 public class ExampleMod {
@@ -58,174 +45,29 @@ public class ExampleMod {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // register new material
-    public static final ToolMaterial GOD_TIER = new ToolMaterial(
-        BlockTags.INCORRECT_FOR_NETHERITE_TOOL, // Least restrictive (or define your own)
-        9999,   // durability
-        100.0F, // speed
-        25.0F,  // attack bonus
-        30,     // enchantability
-        ItemTags.PLANKS // Repairable with wood planks (for fun)
-    );
+    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    // BLOCKS
+    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // NOTE: Copy one of these blocks to make a new one
-    public static final DeferredBlock<Block> CHEESE_BLOCK = BLOCKS.registerSimpleBlock("cheese_block",
-        BlockBehaviour.Properties.of()
-            .mapColor(MapColor.COLOR_YELLOW)
-            .strength(0.5f, 6.0f) // Hardness, Resistance
-            .lightLevel(state -> 4) // Emits light level 10
-            .sound(net.minecraft.world.level.block.SoundType.STONE) // Stone sound when stepped on or broken
-    );
-
-    public static final DeferredItem<BlockItem> CHEESE_BLOCK_ITEM =
-            ITEMS.registerSimpleBlockItem("cheese_block", CHEESE_BLOCK);
-
-    public static final DeferredBlock<Block> RUBIKS_BLOCK = BLOCKS.registerSimpleBlock("rubiks_block",
-        BlockBehaviour.Properties.of()
-            .mapColor(MapColor.COLOR_BLUE)
-            .strength(0.5f, 6.0f) // Hardness, Resistance
-            .lightLevel(state -> 4) // Emits light level 10
-            .sound(net.minecraft.world.level.block.SoundType.STONE) // Stone sound when stepped on or broken
-    );
-    public static final DeferredItem<BlockItem> RUBIKS_BLOCK_ITEM =
-            ITEMS.registerSimpleBlockItem("rubiks_block", RUBIKS_BLOCK);
-
-    public static final DeferredBlock<Block> CORGI_DISPENSER_BLOCK = BLOCKS.registerSimpleBlock("corgi_dispenser_block",
-        BlockBehaviour.Properties.of()
-            .mapColor(MapColor.COLOR_YELLOW)
-            .strength(0.5f, 6.0f) // Hardness, Resistance
-            .lightLevel(state -> 4) // Emits light level 10
-            .sound(net.minecraft.world.level.block.SoundType.STONE) // Stone sound when stepped on or broken
-    );
-
-    public static final DeferredItem<BlockItem> CORGI_DISPENSER_BLOCK_ITEM =
-        ITEMS.registerSimpleBlockItem("corgi_dispenser_block", CORGI_DISPENSER_BLOCK);
-
-    public static final DeferredBlock<Block> BOOM_BLOCK = BLOCKS.register(
-        "boom_block",
-        registryName -> new BoomBlock(BlockBehaviour.Properties.of()
-            .strength(1f, 30f) // hardness and explosive resistance
-            .speedFactor(2f)
-            .lightLevel(state -> 10)
-            .setId(ResourceKey.create(Registries.BLOCK, registryName))
-        ));
-
-    public static final DeferredItem<BlockItem> BOOM_BLOCK_ITEM =
-        ITEMS.registerSimpleBlockItem("boom_block", BOOM_BLOCK);
-
-    public static final DeferredItem<Item> OP_PICKAXE =
-        ITEMS.register("op_pickaxe", registryName ->
-            new Item(new Item.Properties()
-                .setId(ResourceKey.create(Registries.ITEM, registryName))
-                .durability(9999)
-                .pickaxe(
-                    GOD_TIER,
-                    99.0F,   // Attack Damage
-                    1.6F     // Attack Speed
-                )
-            )
-        );
-
-    public static final DeferredItem<Item> DUPONT_PICKAXE =
-        ITEMS.register("dupont_pickaxe", registryName ->
-            new Item(new Item.Properties()
-                .setId(ResourceKey.create(Registries.ITEM, registryName))
-                .durability(9999)
-                .pickaxe(
-                    GOD_TIER,
-                    99.0F,   // Attack Damage
-                    1.6F     // Attack Speed
-                )
-            )
-        );
-
-    public static final DeferredItem<Item> CHEESE_ITEM =
-        ITEMS.register("cheese", registryName ->
-        new CheeseItem(new Item.Properties()
-            .setId(ResourceKey.create(Registries.ITEM, registryName))
-            .food(new FoodProperties.Builder()
-                .nutrition(4)
-                .saturationModifier(0.5f)
-                .alwaysEdible()
-                .build()
-            )
-        )
-    );
-
-    public static final DeferredItem<Item> TRANSMUTATION_WAND_HANDLE =
-		ITEMS.register("transmutation_wand_handle", registryName ->
-		    new Item(new Item.Properties()
-		        .setId(ResourceKey.create(Registries.ITEM, registryName))
-		    )
-		);
-
-    public static final DeferredItem<Item> TRANSMUTATION_WAND_HEAD =
-        ITEMS.register("transmutation_wand_head", registryName ->
-            new Item(new Item.Properties()
-                .setId(ResourceKey.create(Registries.ITEM, registryName))
-            )
-        );
-        
-    public static final DeferredItem<Item> TRANSMUTATION_WAND =
-        ITEMS.register("transmutation_wand", registryName ->
-            new Item(new Item.Properties()
-                .setId(ResourceKey.create(Registries.ITEM, registryName))
-            )
-        );
-
-    // sounds
-    public static final DeferredRegister<SoundEvent> SOUNDS =
-            DeferredRegister.create(Registries.SOUND_EVENT, "examplemod");
-
-    public static final DeferredHolder<SoundEvent, SoundEvent> CORGI_BARK =
-        SOUNDS.register("corgi_bark", () ->
-            SoundEvent.createVariableRangeEvent(
-                ResourceLocation.fromNamespaceAndPath("examplemod",
-                    "corgi_bark")));
-
-    // CREATIVE TABS
-
-    // NOTE: Add your block item below like "output.accept(CHEESE_BLOCK_ITEM.get())"
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB =
-            CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-                    .title(Component.translatable("itemGroup.examplemod")) // The translation key for the tab title
-                    .withTabsBefore(CreativeModeTabs.COMBAT)
-                    .icon(() -> CORGI_DISPENSER_BLOCK_ITEM.get().getDefaultInstance())
-                    .displayItems((parameters, output) -> {
-                        output.accept(CHEESE_BLOCK_ITEM.get()); // Your custom block item
-                        output.accept(RUBIKS_BLOCK_ITEM.get());
-                        output.accept(CORGI_DISPENSER_BLOCK_ITEM.get());
-                        output.accept(BOOM_BLOCK_ITEM.get());
-                        output.accept(OP_PICKAXE.get());
-                        output.accept(CHEESE_ITEM.get());
-                        output.accept(DUPONT_PICKAXE.get());
-                        output.accept(TRANSMUTATION_WAND.get());
-                        output.accept(TRANSMUTATION_WAND_HANDLE.get());
-                        output.accept(TRANSMUTATION_WAND_HEAD.get());
-                    }).build());
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register our custom block drop handler        
-        NeoForge.EVENT_BUS.register(BlockDropHandler.class);
-        // Register our interactions handler        
-        NeoForge.EVENT_BUS.register(PlayerInteractions.class);
-
-        // Register your sounds
-        SOUNDS.register(modEventBus);
-
-        // Register your keybinding listener to mod event bus
-		Keybinder.register(modEventBus);
-		
-		// Register game event listener
-		Keybinder.registerGameEvents();
 
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
@@ -238,9 +80,6 @@ public class ExampleMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
-//        NeoForge.EVENT_BUS.register(BlockDropHandler.class);
-//        NeoForge.EVENT_BUS.register(PlayerInteractions.class);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -265,7 +104,7 @@ public class ExampleMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(CHEESE_BLOCK_ITEM);
+            event.accept(EXAMPLE_BLOCK_ITEM);
         }
     }
 
